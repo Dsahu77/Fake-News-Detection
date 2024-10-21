761957d5b94d48b6ce23de97b3bf140a698a98a9
Dsahu77 <daisysahu77@gmail.com>
2024-10-21 13:47:03 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import pickle
+
+
+#Loading model and vectorizer:
+
+model_filename = 'pickled_final_model.pkl' 
+with open(model_filename, 'rb') as file:
+    model = pickle.load(file)
+
+with open('pickled_vectorizer.pkl', 'rb') as vec_file:
+    vectorizer = pickle.load(vec_file)
+
+#Website 
+
+st.title('Fake News Detection Application')
+input_text = st.text_input('Enter the title and/or content of the news article:')
+
+ps = PorterStemmer()
+def stemming(text):
+    stemmed_text = re.sub('[^a-zA-Z]'," ", text)
+    stemmed_text = stemmed_text.lower()
+    stemmed_text = stemmed_text.split()
+    stemmed_text = [ps.stem(word) for word in stemmed_text if not word in stopwords.words('english')]
+    stemmed_text = ' '.join(stemmed_text)
+    print("data preproccessed")
+    print(stemmed_text)
+    return stemmed_text
+
+def prediction(input_text):
+    input_text = stemming(input_text)
+    input_data = vectorizer.transform([input_text])
+    prediction = model.predict(input_data)
+    return prediction[0]
+
+if input_text:
+    pred = prediction(input_text)
+    if pred == 1:
+        st.write('The News is Fake!')
+    else:
+        st.write('The News is Real!')
---
+++ readme.md
@@ -0,0 +1,5 @@
+I have created a fake news detection model in this project.
+It is an end to end NLP project.
+First I have created an ipynb file where I am preprocessing the data and then training the model using logistic regression.
+Then I have stored the model and vectorizer in pkl files and made app.py using streamlit .
+On this website, you can enter a news article title and/or it's content and the model will try to accurately predict whether the news article is fake or not.
